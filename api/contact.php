596c370fd155ee6712731e33b1a6892e32d9b4e3
Marko Mackic <markomackic@gmail.com>
2024-06-18 22:37:36 +0200

    small fix

--- api/contact.php
+++ api/contact.php
@@ -1,7 +1,7 @@
 <?php
 
-if (isset($_POST['contact_text']) && isset($_ENV['DSOC_CONTACT'])) {
-    mail($_ENV['DSOC_MAIL'], "Inquiry", strip_tags($_POST['contact_text']));
+if (isset($_POST['contact_text']) && isset($_SERVER['DSOC_CONTACT'])) {
+    mail($_SERVER['DSOC_MAIL'], "Inquiry", strip_tags($_POST['contact_text']));
 }
 
 header("Location: ../index.html");
