bb2e3de2f28f8604f9f8a18dba1c672d300334c1
Marko Mackic <markomackic@gmail.com>
2024-06-18 22:50:14 +0200

    check empty

--- api/contact.php
+++ api/contact.php
@@ -1,6 +1,6 @@
 <?php
 
-if (isset($_POST['contact_text']) && isset($_SERVER['DSOC_MAIL'])) {
+if (isset($_POST['contact_text']) && !empty($_POST['contact_text']) && isset($_SERVER['DSOC_MAIL'])) {
     mail($_SERVER['DSOC_MAIL'], "Inquiry", strip_tags($_POST['contact_text']));
 }
 
